28f5f8afc240fecd4d2654edbadb71511f27981b
santos <santos.sanchez@bue.edu.ar>
2023-12-05 20:37:06 -0300

    java/poo clase9

---
+++ 06_pooJava/clase 9/clase/src/main/java/prueba/objetos.java
@@ -0,0 +1,14 @@
+package prueba;
+
+import model.Orador;
+
+public class objetos {
+	public static void main(String[] args) {
+		Orador ora=new Orador();
+		ora.setId(1);
+		ora.setNombre("juan");
+		ora.setApellido("suarez");
+		
+		ora.getId();
+	}
+}
